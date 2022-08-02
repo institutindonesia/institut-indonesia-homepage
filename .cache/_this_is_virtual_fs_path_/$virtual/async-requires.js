8a0a390f53679e9e492f1d87ce773bac8663ce51
institutindonesia <96043576+institutindonesia@users.noreply.github.com>
2022-08-02 11:53:43 +0800

    Delete .cache directory

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++
@@ -1,8 +0,0 @@
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-jsx": () => import("./../../../src/pages/404.jsx" /* webpackChunkName: "component---src-pages-404-jsx" */),
-  "component---src-pages-english-jsx": () => import("./../../../src/pages/english.jsx" /* webpackChunkName: "component---src-pages-english-jsx" */),
-  "component---src-pages-index-jsx": () => import("./../../../src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */),
-  "component---src-pages-teknologi-jsx": () => import("./../../../src/pages/teknologi.jsx" /* webpackChunkName: "component---src-pages-teknologi-jsx" */)
-}
-
